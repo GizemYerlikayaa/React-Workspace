3a1bc87d89ddd117aab590dbd861f120a4a81271
GizemYerlikayaa <giizemyerlikaya@gmail.com>
2025-07-21 11:55:49 +0300

    Redux Toolkit Http istek kısmını ekledim

--- 11.redux/src/App.jsx
+++ 11.redux/src/App.jsx
@@ -2,19 +2,21 @@ import { useState } from "react";
 import "./App.css";
 import { useSelector, useDispatch } from "react-redux";
 import { decrement, increment } from "./redux/counterSlice";
+import UserList from "./UserList";
 
 function App() {
-  const dispatch = useDispatch();
-  const { value } = useSelector((store) => store.counter);
+  // const dispatch = useDispatch();
+  // const { value } = useSelector((store) => store.counter);
 
   return (
     <>
       <div>
-        <div>{value}</div>
+        {/* <div>{value}</div>
         <div>
           <button onClick={() => dispatch(increment())}>Arttır</button>
           <button onClick={() => dispatch(decrement())}>Azalt</button>
-        </div>
+        </div> */}
+        <UserList />
       </div>
     </>
   );
