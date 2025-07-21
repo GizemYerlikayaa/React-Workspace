50ef9156f2029d2fb4aff0a3cef1453463cff980
GizemYerlikayaa <giizemyerlikaya@gmail.com>
2025-07-21 16:56:38 +0300

    useParams ve useNavigate eklendi

--- 12.router-dom/src/App.jsx
+++ 12.router-dom/src/App.jsx
@@ -3,12 +3,13 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
-import Product from "./pages/Product";
+import Product from "./pages/Products";
 import Contact from "./pages/Contact";
 import NotFoundPage from "./pages/NotFoundPage";
 import Header from "./components/Header";
 import EmployeeAbout from "./pages/EmployeeAbout";
 import CompanyAbout from "./pages/CompanyAbout";
+import ProductDetails from "./pages/ProductDetails";
 
 function App() {
   return (
@@ -22,6 +23,7 @@ function App() {
         </Route>
 
         <Route path="/product" element={<Product />} />
+        <Route path="/product-details/:id" element={<ProductDetails />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
---
+++ 12.router-dom/src/data/products.jsx
@@ -0,0 +1,17 @@
+export const products = [
+  {
+    id: "1",
+    name: "Bilgisayar",
+    price: "150.000",
+  },
+  {
+    id: "2",
+    name: "Telefon",
+    price: "95.000",
+  },
+  {
+    id: "3",
+    name: "Televizyon",
+    price: "65.000",
+  },
+];
