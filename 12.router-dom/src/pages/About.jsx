bf0bf77859788a4c021e9b9d486468fcd51cc6da
GizemYerlikayaa <giizemyerlikaya@gmail.com>
2025-07-21 15:10:07 +0300

    İc İce Routerler Eklendi

--- 12.router-dom/src/pages/About.jsx
+++ 12.router-dom/src/pages/About.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { Link, Outlet } from "react-router-dom";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div>
+      <h1>About</h1>
+      <hr />
+      <Link to="employee">Çalışanlar Hakkında</Link>
+      <Link to="company">Şirket Hakkında</Link>
+      <Outlet />
+    </div>
+  );
 }
 
 export default About;
