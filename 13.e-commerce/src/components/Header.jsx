689fafc7d98295c98ea85538b340b05498fa267e
GizemYerlikayaa <giizemyerlikaya@gmail.com>
2025-07-22 15:56:11 +0300

    :)

---
+++ 13.e-commerce/src/components/Header.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import "../css/Header.css";
+import { FaShoppingBasket } from "react-icons/fa";
+import { FaRegSun } from "react-icons/fa6";
+import { FaRegMoon } from "react-icons/fa";
+
+function Header() {
+  const [theme, setTheme] = useState(true);
+  const changeTheme = () => {
+    const root = document.getElementById("root");
+
+    if (theme) {
+      root.style.backgroundColor = "black";
+      root.style.color = "white";
+    } else {
+      root.style.backgroundColor = "white";
+      root.style.color = "black";
+    }
+    setTheme(!theme);
+  };
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+      }}
+    >
+      <div className="flex-row">
+        <img className="logo" src="./src/images/e.png" alt="" />
+        <p className="logo-title">Fashion</p>
+      </div>
+      <div className="flex-row">
+        <input
+          className="search-input"
+          type="text"
+          name=""
+          id=""
+          placeholder="Ara.."
+        />
+        <div>
+          <FaShoppingBasket className="icon-sepet" />
+          {theme ? (
+            <FaRegSun className="icon" onClick={changeTheme} />
+          ) : (
+            <FaRegMoon className="icon" onClick={changeTheme} />
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
